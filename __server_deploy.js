c1448d0b96a0e58f1fcf4a81488f0ac59e5a2d58
areezalamin <areezalamin5999@gmail.com>
2023-08-06 14:22:14 +0600

    ending

---
+++ __server_deploy.js
@@ -0,0 +1,49 @@
+/**
+ * --------
+ * Heroku deploy
+ * ------------
+ * ------------------
+ * ONE TIME for any computer
+ * -----------------
+ * 1.Create heroku account.
+ * 2.Verify email.
+ * 3.Install heroku cli.
+ * 4.heroku login.
+ * ----------------
+ * For each project one time
+ * ---------------
+ * 1.heroku create
+ * 2.make sure : git add . git commit. git push
+ * 
+ */
+
+/**
+ * ----------
+ * Vercel deploy
+ * ----------
+ * ---------
+ * in terminal
+ * ---------
+ * 1.npm command
+ * 2.vercel login
+ * ----------
+ * 3.create vercel.json in project
+ * 4.copy last code from https://shadowsmith.com/thoughts/how-to-deploy-an-express-api-to-vercel
+ * 5.paste it in vercel.json
+ * 6.remove comment.
+ * 7.remove @now from use and add @vercel .
+ * 
+ * -------
+ * again terminal
+ * ------------
+ * 8.vercel
+ * 9.Set up and deploy “C:\Projects\genius-car-service-server”? [Y/n] y
+? Which scope do you want to deploy to? areezalamin
+? Link to existing project? [y/N] n
+? What’s your project’s name? genius-car-service-server
+? In which directory is your code located? ./
+ * 
+ * 
+ * 
+ * 
+ */
